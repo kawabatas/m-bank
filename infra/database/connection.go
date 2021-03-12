2b643a541ff812a5d624c5e2f5c44a449a08f5c1
kawabata_shintaro <4499135+kawabatas@users.noreply.github.com>
2021-03-12 16:47:43 +0900

    Add infra/database package

---
+++ infra/database/connection.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"fmt"
+	"strings"
+)
+
+// DSN create MySQL Data Source Name.
+func DSN(host, user, password, dbname string) string {
+	if strings.HasPrefix(host, "/") {
+		// unix socket
+		return fmt.Sprintf("%s:%s@unix(%s)/%s?charset=utf8mb4&parseTime=true&interpolateParams=true", user, password, host, dbname)
+	}
+	// tcp
+	return fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=true&interpolateParams=true", user, password, host, dbname)
+}
+
+// TestDBName returns test db name.
+func TestDBName(dbname string) string {
+	return fmt.Sprintf("%s_test", dbname)
+}
